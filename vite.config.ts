406bb37538099fa3d2ce48462e155fabdf66d6bd
LSH9132 <me.lshworkspace.com>
2025-05-08 15:44:37 +0900

    molla

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/',
+  plugins: [
+    react()
+  ],
+  build: {
+    outDir: 'dist',
+    ssrManifest: true,
+  }
 })

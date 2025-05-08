ca601f396c860ea8764de95365ad0862bb24f416
LSH9132 <me.lshworkspace.com>
2025-05-08 16:03:09 +0900

    edit: workflow

--- src/entry-server.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { renderToStaticMarkup } from 'react-dom/server'
-import App from './App'
-
-export function render() {
-  const html = renderToStaticMarkup(<App />)
-  return { html }
-} 

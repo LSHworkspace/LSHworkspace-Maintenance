074c85c8b0490555e3ea444fc647a0a5ae18e2d6
Seunghee Lee <lsh@Seunghees-Mac-Studio.local>
2025-05-15 16:58:56 +0900

    feat: deploy two ways

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://maintenance.lshworkspace.com/',
   plugins: [
     react(),
     tailwindcss(),
@@ -12,5 +12,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     ssrManifest: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   }
 })
